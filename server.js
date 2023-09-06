07f6fb125bf4c00f3c914edfa3ef5b3f496b0aa0
Bayram Çınar <99193151+bayramcinar@users.noreply.github.com>
2023-09-06 18:10:25 +0300

    server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 const app = express();
 app.use(cors({
-  origin:["https://restaurantmenu.onrender.com/masa/10"],
+  origin:["https://restaurantmenu.onrender.com"],
   methods: ["POST","GET","DELETE"],
   credentials: true
 }));
